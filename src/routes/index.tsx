971f656a379572098743ce61a18e87a763b37481
Prince Raj <profile.princeraj@gmail.com>
2026-02-04 00:15:24 +0530

    style: update background color for RootDocument and adjust padding in Home component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,7 +9,7 @@ function Home() {
     const state = Route.useLoaderData();
 
     return (
-        <div className="bg-gray-700 w-screen h-screen flex justify-center items-center">
+        <div className="w-screen h-screen flex justify-center items-center sm:px-4 md:px-40">
             <button
                 className="text-2xl text-white bg-amber-700 px-4 py-2 rounded-2xl shadow outline-white outline-2 hover:bg-amber-800 transition hover:cursor-pointer"
                 onClick={() => router.navigate({ to: "/tool" })}>

24fb74384e39d868773feca5c202c820ee6a00e1
theprinceraj <profile.princeraj@gmail.com>
2026-03-11 01:49:50 +0530

    feat: integrate Nitro plugin into Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
     server: {
@@ -16,6 +17,7 @@ export default defineConfig({
                 enabled: true,
             },
         }),
+        nitro(),
         // react's vite plugin must come after start's vite plugin
         viteReact(),
     ],
